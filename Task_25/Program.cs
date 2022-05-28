a83dbdbb6f9c673c57ce504963a2243daed7c96e
ELIZA <rysaeva96@mail.ru>
2022-05-28 11:14:18 +0500

    Решение 25 задачи

---
+++ Task_25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите метод, который принимает на вход
+// два числа (A и B) и возводит число A в натуральную
+// степень B. Задачу решить без использования Math.Pow.
+
+int Method1(int result)
+{
+    Console.WriteLine("Введите число А: ");
+    int numA = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите число B: ");
+    int numB = int.Parse(Console.ReadLine());
+
+    result = 1;
+    for (int i = 0; i < numB; i++)
+    {
+        result = result * numA;
+    
+    }
+    Console.Write("Результат: ");
+    Console.WriteLine(result);
+    return result;
+
+}
+
+int result = Method1(result: 1);
